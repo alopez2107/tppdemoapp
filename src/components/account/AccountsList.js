ac8851caa66e24cf50db0931e67014382510c637
alopez2107 <31291882+alopez2107@users.noreply.github.com>
2024-08-01 17:53:11 -0500

    Fixed an error displaying accounts

--- src/components/account/AccountsList.js
+++ src/components/account/AccountsList.js
@@ -5,13 +5,6 @@ import Accounts from "./Accounts";
 
 
 const AccountsList = (props) => { 
-    const AccountsList = hasFetched ? (<Accounts accounts={accounts}/>):(
-        <div className="accounts-list__header">
-        <h2>Loading your accounts, please wait...</h2>
-      </div>
-    );
-
-    return AccountsList;
     return (
       <Accounts accounts={props.accounts}/>
     );
